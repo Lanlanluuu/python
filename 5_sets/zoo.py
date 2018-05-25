f180480ff0087daa11712feac246d44bc8769475
Lanlanluuu <xww1748@gmail.com>
2018-05-25 17:34:47 +0800

    5/25

---
+++ 5_sets/zoo.py
@@ -0,0 +1,21 @@
+students = [2000,1900,1880,1700]
+
+ages = {
+    "Alice":15,
+    "Jimmy":17,
+    "Griffin":35
+}
+
+zooA = {'elephant', 'tiger', 'lion', 'giraffe', 'leopard', 'antelop'}
+zooB = {'lion', 'cheetah', 'giraffe', 'zebra', 'wildebeest', 'elephant'}
+
+print("zoo A:",zooA)
+print("zoo B:",zooB)
+print("zoo A and zoo B:",zooA & zooB)
+# 都有的
+print("zoo A or zoo B:",zooA | zooB)
+# 
+print("zoo A - zoo B:",zooA - zooB)
+# 
+print("zoo A xor zoo B:",zooA ^ zooB)
+# 兩邊都有的不會出現
