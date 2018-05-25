22c4c3c05bc31021e1cc7c0c2c96a36a2fa12241
Lanlanluuu <xww1748@gmail.com>
2018-05-25 17:30:17 +0800

    5/25

---
+++ 4_dict/dict_1.py
@@ -0,0 +1,21 @@
+ages={}
+
+def prAge(name):
+    
+
+def test():
+    num = int (input())
+    # print(num)
+    for i in range(0.num):
+        line=input()
+        # print(line)
+        tokens=line.split()
+        # print(tokens)
+        ages[tokens[0]] = int(tokens[1])
+        # print(ages)
+    num = input()
+    # print(num)
+    for i in range(0,num):
+        name = input()
+        # print(name)
+        # print(ages.get(name))
